8f16f208ac73a3aabe76605f556c485c4f9f474e
sovisrushain <sovisrushain@gmail.com>
2023-08-01 08:39:45 +0530

    validations

--- recharge-service/src/main/java/com/rechargeservice/controller/RechargeController.java
+++ recharge-service/src/main/java/com/rechargeservice/controller/RechargeController.java
@@ -2,11 +2,15 @@ package com.rechargeservice.controller;
 
 import com.rechargeservice.dto.Pkg;
 import com.rechargeservice.service.RechargeService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/recharge")
@@ -16,7 +20,10 @@ public class RechargeController {
     private final RechargeService rechargeService;
 
     @PostMapping
-    public String packageRecharge(@RequestBody Pkg pkg) {
+    public String packageRecharge(@Valid @RequestBody Pkg pkg, Errors errors) {
+        if(errors.hasErrors()) {
+            return Objects.requireNonNull(errors.getFieldError()).getDefaultMessage();
+        }
         System.out.println(pkg.getAmount());
         return rechargeService.packageRecharge(pkg);
     }
--- recharge-service/src/main/java/com/rechargeservice/dto/Pkg.java
+++ recharge-service/src/main/java/com/rechargeservice/dto/Pkg.java
@@ -1,5 +1,7 @@
 package com.rechargeservice.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Pkg {
+    @NotNull(message = "Number can not be empty")
+    @Pattern(regexp = "[0-9]", message = "Invalid mobile number")
     private String number;
+    @NotNull(message = "Amount can not be empty")
+    @Pattern(regexp = "[0-5]]", message = "Invalid amount")
     private String amount;
 }
